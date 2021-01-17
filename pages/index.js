749464bafba97813b7d9691018b221ecdf51a574
jongwon <54282927+loosie@users.noreply.github.com>
2021-01-17 18:27:07 +0900

    server set

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import AppLayout from '../components/AppLayout';
 import React from 'react'
 
-
 const Home = () => {
 
     return(
--- server.js
+++ server.js
@@ -5,40 +5,43 @@ const bodyParser = require('body-parser')
 const mailer = require('./mail/mailer');
 
 const dev = process.env.NODE_ENV !== 'production'
-// const prod = process.env.NODE_ENV === 'production'
+
+console.log(dev);
 const app = next({ dev })
-// const prodApp = next({ prod })
 const handle = app.getRequestHandler()
 
 
-app.prepare().then(() => {
-// prodApp.prepare().then(() => {
-  const server = express()
-
-  server.use(bodyParser.json())
-
-  server.post('/api/contact', (req, res) => {
-    const { email = '', name ='', message='', title='', phoneNumber='' } = req.body;
-    console.log(req.body);
-
-    mailer({ email, name, text: message, title, phoneNumber })
-    .then(() => {
-        console.log('success');
-        res.send('success');
+if(process.env.NODE_ENV !== 'production'){
+  app.prepare().then(() => {
+      const server = express()
+    
+      server.use(bodyParser.json())
+    
+      server.post('/api/contact', (req, res) => {
+        const { email = '', name ='', message='', title='', phoneNumber='' } = req.body;
+        console.log(req.body);
+    
+        mailer({ email, name, text: message, title, phoneNumber })
+        .then(() => {
+            console.log('success');
+            res.send('success');
+        })
+        .catch((err) => {
+            console.log('failed :', err);
+            res.send('badddd');
+        })
+      })
+    
+      server.get('*', (req, res) => {
+        return handle(req, res)
+      })
+    
+      server.listen(3000, (err) => {
+        if (err) throw err
+        console.log('> Read on http://localhost:3000')
+      })
     })
-    .catch((err) => {
-        console.log('failed :', err);
-        res.send('badddd');
-    })
-  })
+}
 
-  server.get('*', (req, res) => {
-    return handle(req, res)
-  })
 
-  server.listen(3000, (err) => {
-    if (err) throw err
-    console.log('> Read on http://localhost:3000')
-  })
-})
 
