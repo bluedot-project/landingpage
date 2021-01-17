835d65d09c578591225923c7c7e239f3441aa9a4
jongwon <54282927+loosie@users.noreply.github.com>
2021-01-17 22:50:02 +0900

    title수정

--- pages/_document.js
+++ pages/_document.js
@@ -42,7 +42,7 @@ export default class MyDocument extends Document{
 
                     <meta property="og:type" content="website" />
                     <meta property="og:url" content="http://13.125.206.184/" />
-                    <meta property="og:title" content="아티스트 브랜딩 콘텐츠 커뮤니티 플랫폼 | 블루닷 " />
+                    <meta property="og:title" content="아티스트 브랜딩 콘텐츠 커뮤니티 플랫폼 | 블루닷 Bludeot (2021) " />
                     <meta property="og:image" content="/bdot_logo2.png" />
                     <meta property="og:description" content="팬 소통과 수익 창출을 함께 하는 새로운 아티스트 퍼스널브랜딩 커뮤니티" />
                     
